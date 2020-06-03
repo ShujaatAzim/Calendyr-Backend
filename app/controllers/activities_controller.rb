cf067eb7aec09f4c6dbe7ef23d27ddffab7ac122
Shujaat Azim <shujaat.azim@gmail.com>
2020-06-02 23:59:06 -0400

    Built out the basics of the back end, like CORS and the one model. Connected to front end successfully.

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,8 @@
+class ActivitiesController < ApplicationController
+
+  def index
+    @activities = Activity.all
+    render json: @activities.to_json()
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+print "Seeding..."
+
+activity01 = Activity.create(date: Date.today, name: "Programming", complete: false)
+activity02 = Activity.create(date: Date.today, name: "Japanese", complete: false)
+activity03 = Activity.create(date: Date.today, name: "Chess", complete: false)
+activity04 = Activity.create(date: Date.today, name: "Exercise", complete: false)
+
+activity05 = Activity.create(date: Date.tomorrow, name: "Programming", complete: false)
+activity06 = Activity.create(date: Date.tomorrow, name: "Japanese", complete: false)
+activity07 = Activity.create(date: Date.tomorrow, name: "Chess", complete: false)
+activity08 = Activity.create(date: Date.tomorrow, name: "Exercise", complete: false)
+
+print "Done!"
