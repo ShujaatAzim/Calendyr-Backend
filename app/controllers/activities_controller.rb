8c35005ad414923115ce4059a573d18145097c2d
Shujaat Azim <shujaat.azim@gmail.com>
2020-06-03 23:41:40 -0400

    Added a serializer for activities. Works properly.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,12 @@ class ActivitiesController < ApplicationController
 
   def index
     @activities = Activity.all
-    render json: @activities.to_json()
+    render json: ActivitySerializer.new(@activities).to_serialized_json
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
+    render json: ActivitySerializer.new(@activity).to_serialized_json
   end
 
 end
