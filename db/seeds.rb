8209125e578d5a3986a5c024195a31516a2c7720
Shujaat Azim <shujaat.azim@gmail.com>
2021-07-12 14:01:58 -0400

    Added new seed data but this will probably not end up being the backend for this app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ activity02 = Activity.create(date: Date.today, name: "Japanese", complete: true)
 activity03 = Activity.create(date: Date.today, name: "Chess", complete: true)
 activity04 = Activity.create(date: Date.today, name: "Exercise", complete: true)
 
-activity05 = Activity.create(date: Date.tomorrow, name: "Programming", complete: false)
-activity06 = Activity.create(date: Date.tomorrow, name: "Japanese", complete: false)
-activity07 = Activity.create(date: Date.tomorrow, name: "Chess", complete: false)
-activity08 = Activity.create(date: Date.tomorrow, name: "Exercise", complete: false)
+activity05 = Activity.create(date: Date.yesterday, name: "Programming", complete: false)
+activity06 = Activity.create(date: Date.yesterday, name: "Japanese", complete: false)
+activity07 = Activity.create(date: Date.yesterday, name: "Chess", complete: false)
+activity08 = Activity.create(date: Date.yesterday, name: "Exercise", complete: false)
 
 print "Done!"
